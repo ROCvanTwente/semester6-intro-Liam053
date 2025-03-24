15f826a83581fffeb1034fc0fb34e92bb942a09e
Liam053 <Liam@kleinhalle.nl>
2025-03-24 14:59:21 +0100

    Project Made

---
+++ Semester6LiamKleinhalle/Semester6LiamKleinhalle/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Semester6LiamKleinhalle.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
